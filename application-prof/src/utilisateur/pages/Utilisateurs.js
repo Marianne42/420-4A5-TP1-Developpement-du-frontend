aaabb962bacc63d0140259d638b11bd87e316bca
marianne24 <2138283@cmontmorency.qc.ca>
2023-03-13 19:02:56 -0400

    modification css + ajout de donnés test

--- application-prof/src/utilisateur/pages/Utilisateurs.js
+++ application-prof/src/utilisateur/pages/Utilisateurs.js
@@ -8,7 +8,7 @@ const utilisateurs = [
     nom: "Labranche",
     dateEmbauche: new Date("2001-11-28"),
     image: "https://www.chumontreal.qc.ca/sites/default/files/guy-pare_5.jpeg",
-    cours: ["p1", "p3"],
+    cours: ["p1", "p3","p4"],
   },
   {
     id: "u2",
@@ -16,7 +16,7 @@ const utilisateurs = [
     nom: "Chat",
     dateEmbauche: new Date("2015-02-31"),
     image: "https://imgs.search.brave.com/8geawRwEFS-pveoR54T_jcAoclS3C85q9KnyuKyquz0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/dGhlc3BydWNlcGV0/cy5jb20vdGhtYi9o/SjQwaGRwSzRLWjVB/ZWhsTW1WRWRKcjh6/UEk9LzE5OTl4MTQ1/OS9maWx0ZXJzOmZp/bGwoYXV0bywxKS90/d2VudHkyMF9lNDdi/Mzc5OC1kZDliLTQw/YjEtOTFlZi0xZDgy/MDMzNzk2NmUtNWFh/M2Y3OTg2NDJkY2Ew/MDM2M2IwZGYxLmpw/Zw",
-    cours: ["p2"],
+    cours: ["p2","p5","p6"],
   },
 ];
 
