1fa348812ac301fa302915172a10c9b8ed0e19aa
marianne24 <2138283@cmontmorency.qc.ca>
2023-03-14 20:34:54 -0400

    formulaire

--- application-prof/src/App.js
+++ application-prof/src/App.js
@@ -12,8 +12,73 @@ import Cours from "./places/pages/Cours";
 import Place from "./places/pages/components/Place";
 import PageCours from "./places/pages/components/PageCours"
 import Accueil from "./Accueil";
+import { useState } from "react";
+
+export var NB_COURS;
+export var set_NB_COURS;
+export var COURS;
+export var set_COURS;
 
 function App() {
+
+  [NB_COURS, set_NB_COURS] = useState(6);
+  [COURS, set_COURS] = useState([
+  {
+    id: "c1",
+    titre: "Cours de Programmation",
+    discipline: "Le cours qui apprend à faire du code",
+    nbrMaxEtudiant: 30,
+    dateDebut: new Date("2023-01-01"),
+    dateFin: new Date("2023-06-06"),
+    professeur: "u1",
+  },
+  {
+    id: "c3",
+    titre: "Cours de Design",
+    discipline: "Le cours qui apprend à faire du je sais pas quoi",
+    nbrMaxEtudiant: 30,
+    dateDebut: new Date("2023-01-01"),
+    dateFin: new Date("2023-06-06"),
+    professeur: "u1",
+  },
+  {
+    id: "c2",
+    titre: "Toilettage de chat",
+    discipline: "Cours pour apprendre à toiletter son chat trop pipou",
+    nbrMaxEtudiant: 30,
+    dateDebut: new Date("2022-08-01"),
+    dateFin: new Date("2022-12-31"),
+    professeur: "u2",
+  },
+  {
+    id: "c4",
+    titre: "Cours de Guérilande",
+    discipline: "Le cours qui apprend à jouer un deck Guérilande Voodoo Zone perdu",
+    nbrMaxEtudiant: 30,
+    dateDebut: new Date("2022-01-02"),
+    dateFin: new Date("2022-06-05"),
+    professeur: "u3",
+  },
+  {
+    id: "c5",
+    titre: "Cours de sortilèges niveau 1",
+    discipline: "Apprenez dans ce cours les sorts de base comme Levioso ou Imperio",
+    nbrMaxEtudiant: 20,
+    dateDebut: new Date("2021-09-01"),
+    dateFin: new Date("2021-11-15"),
+    professeur: "u2",
+  },
+  {
+    id: "c6",
+    titre: "Apprendre le langage des Pokémons",
+    discipline: "Attrapez les tous au passage",
+    nbrMaxEtudiant: 5,
+    dateDebut: new Date("2022-08-01"),
+    dateFin: new Date("2022-12-31"),
+    professeur: "u3",
+  },
+]);
+
   return (
     <Router>
       <MainNavigation />
--- application-prof/src/utilisateur/pages/Utilisateurs.js
+++ application-prof/src/utilisateur/pages/Utilisateurs.js
@@ -8,7 +8,7 @@ const utilisateurs = [
     nom: "Labranche",
     dateEmbauche: new Date("2001-11-28"),
     image: "https://www.chumontreal.qc.ca/sites/default/files/guy-pare_5.jpeg",
-    cours: ["p1", "p3"],
+    cours: ["c1", "c3"],
   },
   {
     id: "u2",
@@ -16,7 +16,7 @@ const utilisateurs = [
     nom: "Chat",
     dateEmbauche: new Date("2015-02-31"),
     image: "https://imgs.search.brave.com/8geawRwEFS-pveoR54T_jcAoclS3C85q9KnyuKyquz0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/dGhlc3BydWNlcGV0/cy5jb20vdGhtYi9o/SjQwaGRwSzRLWjVB/ZWhsTW1WRWRKcjh6/UEk9LzE5OTl4MTQ1/OS9maWx0ZXJzOmZp/bGwoYXV0bywxKS90/d2VudHkyMF9lNDdi/Mzc5OC1kZDliLTQw/YjEtOTFlZi0xZDgy/MDMzNzk2NmUtNWFh/M2Y3OTg2NDJkY2Ew/MDM2M2IwZGYxLmpw/Zw",
-    cours: ["p2","p5"],
+    cours: ["c2","c5"],
   },
   {
   id: "u3",
@@ -24,7 +24,7 @@ const utilisateurs = [
   nom: "De Voodoo",
   dateEmbauche: new Date("2019-01-01"),
   image: "http://pa1.narvii.com/7091/f72a3b4f989e6ac4dba12495cb4a3799e5a830d6r1-150-150_00.gif",
-  cours:["p4","p6"]
+  cours:["c4","c6"]
   }
 ];
 
